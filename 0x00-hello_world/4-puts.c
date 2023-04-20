c2a64654a4d5a275eb885fc0ed6792786e3de58a
Sarahgad <sarahgad91@gmail.com>
2023-04-20 13:23:06 -0700

    ${message}

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @main: describe the main
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	 char Mystr[] = "\"Programming is like building a multilingual puzzle";
-    
-    puts(Mystr);
-    
-	return (0);
-
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+puts(Mystr);
+return (0);
 }
