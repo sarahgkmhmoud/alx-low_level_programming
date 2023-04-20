a49e38ba58fe67de27ccbf0551fdaa5a8cf72f34
Sarahgad <sarahgad91@gmail.com>
2023-04-20 12:43:27 -0700

    ${message}

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	 char Mystr[] = "Programming is like building a multilingual puzzle";
+    
+    puts(Mystr); //writing the string to stdout
+    
+	return (0);
+
+}
