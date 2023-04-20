2c603484beeb4138c5ab80bb0a4d66a8418c82da
Sarahgad <sarahgad91@gmail.com>
2023-04-20 13:19:51 -0700

    ${message}

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * main: describe the main
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
