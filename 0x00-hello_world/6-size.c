69fc8a57d867ebed3a308fae3708f5e43f77be43
Sarahgad <sarahgad91@gmail.com>
2023-04-20 15:13:45 -0700

    ${message}

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,10 @@
  */
 int main(void)
 {
-char c;
-int i;
-double d;
-float f;
-printf("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of an int:%lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int:%lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long long int:%lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float:%lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", (sizeof(long));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(double));
+printf("Size of a float: %zu byte(s)\n", (sizeof(float));
 return (0);
 }
