9576c45c434e0aa95cb9a318b75ebe390355173f
Sarahgad <sarahgad91@gmail.com>
2023-04-21 20:12:15 -0700

    ${message}

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, x);
 }
 else if	(x == 0)
 {
-printf("Last digit of %d is %d and is 0", n, (n % x));
+printf("Last digit of %d is %d and is 0\n", n, x);
 }
 else if (x < 6 && x != 0)
 {
