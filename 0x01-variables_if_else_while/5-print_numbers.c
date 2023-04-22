ac70cc0758f224f88bfa74acc7fa49faed82391d
Sarahgad <sarahgad91@gmail.com>
2023-04-21 19:47:37 -0700

    ${message}

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: 'Alphabet'
+ * c: describe the char
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+char c;
+	c = '0';
+	while (c <= '9')
+{	putchar(c);
+	c++;
+}
+	putchar('\n');
+	return (0);
+}
