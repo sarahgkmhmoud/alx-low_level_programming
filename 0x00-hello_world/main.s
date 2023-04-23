71c209502f063776b670844d8b6bbabe7a17dc15
Sarahgad <sarahgad91@gmail.com>
2023-04-23 09:35:20 -0700

    ${message}

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -4,13 +4,20 @@
 	.globl	main
 	.type	main, @function
 main:
-.LFB23:
+.LFB0:
 	.cfi_startproc
 	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
 	mov	eax, 0
+	pop	rbp
+	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE23:
+.LFE0:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
