a5c6dbc389331da6f190e43dfc9ef25eca361528
Sarahgad <sarahgad91@gmail.com>
2023-04-22 23:43:04 -0700

    ${message}

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,16 +10,12 @@ int main(void)
 {
 int c;
 	c = '0';
-	while (c <= '9')
+	while (c < '9')
 {	putchar(c);
-	if (c == '9')
-{
-	putchar('\n');
-}
-	else
 	putchar(',');
 	putchar(' ');
 	c++;
 }
+	putchar(c);
 	return (0);
 }
