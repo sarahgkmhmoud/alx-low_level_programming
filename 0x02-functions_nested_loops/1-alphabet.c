f29e3fb2ecb1fc5cf31f2f457353d7aa5de72ea4
Sarahgad <sarahgad91@gmail.com>
2023-04-25 13:12:40 -0700

    ${message}

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ * @void: The character to print
+ */
+void print_alphabet(void)
+{
+	_putchar('a');
+	putchar('b');
+	putchar('c');
+	putchar('d');
+	putchar('e');
+	putchar('f');
+	putchar('g');
+	putchar('h');
+	putchar('i');
+	putchar('j');
+	putchar('k');
+	putchar('l');
+	putchar('m');
+	putchar('n');
+	putchar('o');
+	putchar('p');
+	putchar('q');
+	putchar('r');
+	putchar('s');
+	putchar('t');
+	putchar('u');
+	putchar('v');
+	putchar('w');
+	putchar('x');
+	putchar('y');
+	putchar('z');
+	_putchar('\n');
+}
