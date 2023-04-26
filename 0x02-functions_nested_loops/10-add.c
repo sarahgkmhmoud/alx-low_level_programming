0884bf6e884bd90c828bba332d275aec0252f50b
Sarahgad <sarahgad91@gmail.com>
2023-04-25 17:44:14 -0700

    ${message}

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ *add - adds two integers and returns the result.
+ *@x: The intgr to add
+ *@y: The intgr to add
+ *
+ * Return: On success z.
+ */
+int add(int x, int y)
+{
+int z = x + y;
+return (z);
+}
