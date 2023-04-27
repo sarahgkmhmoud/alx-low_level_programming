897fcbd424be783b5282c10273d4058c52fb915b
Sarahgad <sarahgad91@gmail.com>
2023-04-27 03:15:04 -0700

    ${message}

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ *mul - multiply two integers and returns the result.
+ *@a: The intgr to mul
+ *@b: The intgr to mul
+ *
+ * Return: On success z.
+ */
+int mul(int a, int b)
+{
+int z = a * b;
+return (z);
+}
