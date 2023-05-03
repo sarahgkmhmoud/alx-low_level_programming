622a51a96bebd8d448b5249116f64306887d9661
Sarahgad <sarahgad91@gmail.com>
2023-05-03 14:53:21 -0700

    ${message}

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_array - writes the character c to stdout
+ * @a : Decribe the char arry
+ * @n : descrip the integer
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	int *p;
+
+	p = a;
+
+	for (i = 0; i < n; i++)
+	{
+	printf("%d", *(p + i));
+	if (i != n - 1)
+	printf(", ");
+	else
+	printf("\n");
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,3 +8,4 @@ void _puts(char *str);
 void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
