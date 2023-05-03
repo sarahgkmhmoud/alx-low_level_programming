2a81e2d8b510152f5b5eeeaabefacc85804783bd
Sarahgad <sarahgad91@gmail.com>
2023-05-03 07:11:50 -0700

    ${message}

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * _strlen - writes the character c to stdout
+ * @s : Decribe the char arry
+ * Return: On success i.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+		return (i);
+}
