92af34fe98fa3217a0b7f47a15d4a4b568451e90
Sarahgad <sarahgad91@gmail.com>
2023-05-03 12:10:02 -0700

    ${message}

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * puts_half - writes the character c to stdout
+ * @str : Decribe the char arry
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] > '\0'; i++)
+	{
+	}
+	if (i % 2 == 0)
+	for (i = i / 2; str[i] > '\0'; i++)
+		_putchar(str[i]);
+	else
+	for (i = (i - 1) / 2; str[i] > '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
