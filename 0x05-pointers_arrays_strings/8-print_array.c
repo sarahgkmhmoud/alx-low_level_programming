b31f135fa7a8beaf47848e11d9762dc7a748233a
Sarahgad <sarahgad91@gmail.com>
2023-05-03 15:23:37 -0700

    ${message}

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,19 +8,13 @@
 void print_array(int *a, int n)
 {
 	int i;
-	int *p;
 
-	p = a;
-
-	if (sizeof(int *a) == n)
-	{
 	for (i = 0; i < n; i++)
 	{
-	printf("%d", *(p + i));
+	printf("%d", *(a + i));
 	if (i != n - 1)
 	printf(", ");
 	else
 	printf("\n");
-	}
 	}
 }
