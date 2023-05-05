ffcd5ab1efe35e8772cb97a0b1bc731e69de876c
Sarahgad <sarahgad91@gmail.com>
2023-05-04 18:31:14 -0700

    ${message}

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n && src[i] != '\0';  j++)
 	dest[i + j] = src[j];
 	
 	dest[j + i] = '\0';
