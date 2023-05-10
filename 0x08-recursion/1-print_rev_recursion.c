6055612650caf15a9dcfed7d3af2cefbc490510b
Sarahgad <sarahgad91@gmail.com>
2023-05-10 12:16:07 -0700

    ${message}

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * _print_rev_recursion - prints a string, followed by a new line.
+ *@s: The character to print
+ *
+ */
+void  _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
+}
+
