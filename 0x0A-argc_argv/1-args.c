b05b4f8925f227deaa7aefd97e81854ba3a3db2c
Sarahgad <sarahgad91@gmail.com>
2023-05-12 14:01:36 -0700

    ${message}

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - descripe the main function
+ *@argc : decript the count
+ *@argv : desribe the array
+ *Return: 0 always success
+ */
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
