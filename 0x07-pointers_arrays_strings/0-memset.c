c960300fe95f89ce027784f7e65861af086ea0d8
Sarahgad <sarahgad91@gmail.com>
2023-05-12 09:26:46 -0700

    ${message}

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ *_memset -  fills memory with a constant byte.
+ *@s : discripe the array
+ *@b : discripe the array
+ *@n : discripe the array
+ *return : 0 always sucess
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_diagsums - Write a function that prints the sum of the two diagonals of a square matrix of integers.
+ *@a : discripe the array
+ * sixe : descripe the integer
+ *return : 0 always sucess
+ */
+void print_diagsums(int *a, int size)
+{
+}
