225a520b7be2cd071d676a3cb509b690615bd028
Sarahgad <sarahgad91@gmail.com>
2023-05-13 11:54:56 -0700

    ${message}

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ *_strcpy - write  a function that concatenates two strings.
+ * @dest: Describe the string 1
+ * @src: Describe the string 2
+ * Return: Always dest (Success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+}
