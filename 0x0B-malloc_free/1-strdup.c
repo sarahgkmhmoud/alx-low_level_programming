af2f6d58fef46d5e89cd8d9509d8d17daa722606
Sarahgad <sarahgad91@gmail.com>
2023-05-15 05:27:23 -0700

    ${message}

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include"main.h"
+#include <stdlib.h>
+/**
+ *_strdup - contains a copy of the string given
+ *@str: Describe the array
+ *
+ *Return: a always sucsess
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *a;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	a =  malloc(sizeof(*a));
+
+		for (i = 0; str[i] != '\0'; i++)
+		a[i] = str[i];
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	return (a);
+}
