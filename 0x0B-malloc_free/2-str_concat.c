0ee1047681d9841d2bcbcedeb41181ebec26a5b0
Sarahgad <sarahgad91@gmail.com>
2023-05-15 08:25:42 -0700

    ${message}

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,10 +15,10 @@ char *str_concat(char *s1, char *s2)
 
 	n = sizeof(s2) +  sizeof(s1);
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		return (NULL);
-	}
+	if (s1 == NULL)
+		s1 = '\0';
+	if(s2 == NULL)
+		s2 = '\0';
 	
 	a =  malloc(sizeof(n));
 	for (j = 0; s1[j] != '\0'; j++)
