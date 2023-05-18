16a3c684cae9448fea6a436b2b49e4007093f890
Sarahgad <sarahgad91@gmail.com>
2023-05-18 08:08:45 -0700

    ${message}

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"main.h"
+/**
+ *malloc_checked - Write a function that allocates memory using
+ *@b: present the intger
+ *
+ *Return: a always success
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+
+	if (a != NULL)
+		exit(0);
+	else
+		exit(98);
+	return (a);
+}
