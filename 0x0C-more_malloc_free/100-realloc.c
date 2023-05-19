0f53c18e9ebcc7e62c6caa6d31a78d61553ffbd0
Sarahgad <sarahgad91@gmail.com>
2023-05-18 17:56:03 -0700

    ${message}

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"main.h"
+/**
+ *_realloc - Write a function that reallocates a memory block
+ *@ptr: present the intger
+ *@old_size: as a parameter
+ *@new_size: as a parameter
+ *Return: ptr always success
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	ptr = malloc(old_size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+		free(ptr);
+	}
+	if (new_size > old_size)
+	{
+	ptr = malloc(old_size + new_size);
+	return (ptr);
+	if (ptr == NULL)
+	{
+	free(ptr);
+	return (NULL);
+	}
+	}
+	else if (new_size == old_size)
+	{
+	return (ptr);
+	if (ptr == NULL)
+	{
+		return (NULL);
+			free(ptr);
+	}
+	}
+	else if (new_size == 0 && ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+
+	}
+	return (ptr);
+}
