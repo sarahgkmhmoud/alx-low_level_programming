8642cbe1934ec1718e988e8554c159ede172895f
Sarahgad <sarahgad91@gmail.com>
2023-05-19 17:03:25 -0700

    ${message}

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,13 +14,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *a;
 	unsigned int i, j, len;
 
-	if (n >= strlen(s2))
-		n = strlen(s2);
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	if (n >= strlen(s2))
+		n = strlen(s2);
+
 	len = (unsigned int) strlen(s1) + n + 1;
 
 	a = malloc(sizeof(*a) * len);
