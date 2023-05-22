82b062df556af2d24590baaab6bf7495b734d76a
Sarahgad <sarahgad91@gmail.com>
2023-05-22 16:20:59 -0700

    ${message}

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description:  prints the name of the file it was compiled from
+ *Return: 0 aways suceess
+ *
+ *
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
