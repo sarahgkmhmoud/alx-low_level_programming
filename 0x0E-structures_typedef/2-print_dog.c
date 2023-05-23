c9c7ba74b1af2f985eff68102cc1bdf69144e068
Sarahgad <sarahgad91@gmail.com>
2023-05-23 14:51:46 -0700

    ${message}

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,8 +10,12 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 	exit(1);
 
-	if (d->name == NULL || d->age == 0 || d->owner == NULL)
-		printf("(nil)\n");
+	if (d->name == NULL)
+		printf("Name : (nil)\n");
+	if (d->age == 0)
+		 printf("Age: (nil)\n");
+	if ( d->owner == NULL)
+		printf("Owner: (nil)\n");
 
 	printf("Name: %s\n", d->name);
 	 printf("Age: %f\n", d->age);
