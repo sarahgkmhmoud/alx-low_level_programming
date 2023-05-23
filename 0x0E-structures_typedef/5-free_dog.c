e9a29831f39825181ce4fd47bc1d3b129b6617d5
Sarahgad <sarahgad91@gmail.com>
2023-05-23 16:49:45 -0700

    ${message}

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * free_dog - initialize a variable of type struct dog
+ * @d: First member
+ * Description: Longer description
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
