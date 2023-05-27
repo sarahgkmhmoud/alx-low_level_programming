ae27dd645bd2c6c07f138a0e7e37183ca732882e
Sarahgad <sarahgad91@gmail.com>
2023-05-27 10:56:18 -0700

    ${message}

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,33 +2,49 @@
 #include<stdarg.h>
 #include <stdlib.h>
 #include"variadic_functions.h"
-#define int i;
 /**
- *print_strings - print them
- *@separator: string of char
- *@n: const unsigned int
+ *print_all - print them
+ *@format: string of char
  *
  *
  */
 void print_all(const char * const format, ...)
 {
 va_list ptr;
-int i; 
-
-i = 0;
-printf ("%d", i);
-printf ("%s", format);
+int d, i;
+char c, *s;
+double f;
 va_start(ptr, format);
+i = 0;
 while (format[i] != '\0')
 {
-printf("%d\n", va_arg(ptr,format[i]));
-i++;
+	if (format[i] == 'c')
+	{
+	c = (char) va_arg(ptr, int);
+	printf("%c", c);
+	}
+	else if (*(format + i) == 'i')
+	{
+	d = va_arg(ptr, int);
+	printf("%d", d);
+	}
+	else if (*(format + i) == 'f')
+	{
+	f = va_arg(ptr, double);
+	printf("%f", f);
+	}
+	else if (*(format + i) == 's')
+	{
+	s = va_arg(ptr, char*);
+	printf("%s", s);
+	}
+	i++;
+	if (format[i] == '\0')
+		break;
+	if (format[i] == 's' || format[i] == 'i'
+|| format[i] == 'c' || format[i] == 'f')
+	printf(", ");
 }
 printf("\n");
 va_end(ptr);
-}
-
-void main(void)
-{
-print_all("iscf", 4, "school", 'c', 0.0);
 }
