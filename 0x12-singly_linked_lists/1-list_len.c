bb8a65f8bd37022af95594a2f54c48dff0c87f2e
Sarahgad <sarahgad91@gmail.com>
2023-06-01 13:12:29 -0700

    lenth list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -13,15 +13,14 @@ size_t list_len(const list_t *h)
 
 unsigned int count;
 
-if (h == NULL)
-return (-1);
-
-
+if (h != NULL)
+{
 count = 0;
 while (h != NULL)
 {
 count++;
 h = h->next;
+}
 }
 return (count);
 }
