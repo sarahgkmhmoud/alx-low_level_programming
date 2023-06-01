1e22c440619e471601c48a2ed0404b696ad5fb5f
Sarahgad <sarahgad91@gmail.com>
2023-06-01 05:36:29 -0700

    print it

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include"lists.h"
+/**
+*print_list - print the linked lists
+*@h : header pointer
+*Descrption: print the data
+*Return: count always success
+*/
+size_t print_list(const list_t *h)
+{
+
+unsigned int count;
+
+if (h == NULL)
+return (1);
+
+
+count = 0;
+while (h != NULL)
+{
+count++;
+if (h->str != NULL)
+printf("[%d] %s\n", h->len, h->str);
+else
+printf("[0] (nil)\n");
+h = h->next;
+}
+return (count);
+}
