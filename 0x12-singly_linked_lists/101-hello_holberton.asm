c406562714f9a24ca039069b3dda934f5067403d
Sarahgad <sarahgad91@gmail.com>
2023-06-02 13:45:48 -0700

    assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,17 +3,16 @@ extern printf
 global main
 
 main:
-
 push rbp
-mov edi, len
+
+mov rdi, fmt
 mov esi, msg
 mov rax, 0
-call printf
-
-mov rbp
-mov rax, 0
+call printf wrt ..plt
+pop rbp
+mov rax,0
 ret
 
 SECTION .data
-msg: db "Hello, Holberton", 10
-len: db "%s"
+msg: db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
