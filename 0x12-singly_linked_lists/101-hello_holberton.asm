b3d56b7447b9b5bd794fdcbdffea35abc607c714
Sarahgad <sarahgad91@gmail.com>
2023-06-02 13:52:02 -0700

    assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,12 @@
 SECTION .text
-extern printf
-global main
+extern printf ;part tell the ass this sym exist to be refrence later
+global main ;needed for gcc and entrypoit to libc
 
 main:
-push rbp
+push rbp	;pushed on the stack
 
-mov rdi, fmt
-mov esi, msg
+mov rdi, fmt	;parameter for printf
+mov esi, msg	;parameter for printf
 mov rax, 0
 call printf wrt ..plt
 pop rbp
