17fedc78d9a5f03733a5f4699ea713a08ec77c48
Sarahgad <sarahgad91@gmail.com>
2023-06-05 16:01:54 -0700

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,13 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
-
-	while (*head !=  NULL)
+	listint_t *cur = *head;
+	while (cur !=  NULL)
 {
 
-	tmp = *head;
-	*head = tmp->next;
+	tmp = cur;
+	cur= cur->next;
 	free(tmp);
 }
 *head = NULL;
-free(*head);
 }
