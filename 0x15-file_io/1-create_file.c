ac18c627cc705e89708d51fef3ae321a457e6601
Sarahgad <sarahgad91@gmail.com>
2023-07-10 18:34:55 -0700

    edit task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,9 +9,19 @@ int create_file(const char *filename, char *text_content)
 {
 int fd;
 int writes;
+int len;
+
+	if (filename == NULL)
+		return(-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+			len++;
+	}
 
 	fd = open(filename,  O_CREAT | O_RDONLY | O_TRUNC, 0600);
-	writes = write(fd, text_content, strlen(text_content));
+	writes = write(fd, text_content, len);
 	if (fd == -1 || writes == -1)
 	{
 		return (-1);
