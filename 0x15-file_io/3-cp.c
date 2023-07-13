b6ea10c4788e00c140381ccdd25a0d17dfc9c288
Sarahgad <sarahgad91@gmail.com>
2023-07-13 04:32:32 -0700

    edit error message

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	dprintf(STDERR_FILENO,"Usage: cp file_from file_to\n");
 	exit(97);
 	}
 
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
 	do {
 		if (fd1 == -1 || r == -1)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't read from file %s", argv[1]);
+		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
 		free(buff);
 		exit(98);
 	}
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
 
 		if (fd2 == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO,"Error: Can't write to %s", argv[2]);
+			dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
 			free(buff);
 			exit(99);
 		}
