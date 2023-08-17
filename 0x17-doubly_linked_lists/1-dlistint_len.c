3a0accae70da9856b1d7f2f42e50469cb3eef0fe
Sarahgad <sarahgad91@gmail.com>
2023-08-17 12:02:06 -0700

    2

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,16 +1,18 @@
 #include"lists.h"
 /**
-*print_dlistint - print the linked lists
+*dlistint_len - print the linked lists
 *@h : header pointer
 *Descrption: print the data
 *Return: count always success
 */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 	const dlistint_t *curr = h;
 
 	for (; curr != NULL; curr = curr->next, count++)
-	printf("%d\n", curr->n);
+	{
+		;
+	}
 	return (count);
 }
