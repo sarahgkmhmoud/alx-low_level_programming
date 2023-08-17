51df717ae468b77cda1c3829ca16c20798115acc
Sarahgad <sarahgad91@gmail.com>
2023-08-17 15:14:22 -0700

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,24 +14,27 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 	dlistint_t *curr = *h;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-		if (new_node == NULL)
-			return (NULL);
-	new_node->n = n;
-	if (idx == 0)
-{
-	new_node->next = *h;
-	*h = new_node;
-	return (new_node);
-}
-for (i = 1;  i < idx && curr != NULL; i++)
-{
-	if (curr == NULL || curr->next == NULL)
+
+	if (new_node == NULL)
 		return (NULL);
-	curr = curr->next;
+
+	if (h == NULL)
+		return (NULL);
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (i = 1;  i < idx; i++)
+{
+		if (curr == NULL)
+			return (NULL);
+		curr = curr->next;
 }
+	new_node->n = n;
 	new_node->prev = curr;
 	new_node->next = curr->next;
-	curr->next->prev = new_node;
+	if (curr->next != NULL)
+		curr->next->prev = new_node;
 	curr->next = new_node;
 	return (new_node);
 }
