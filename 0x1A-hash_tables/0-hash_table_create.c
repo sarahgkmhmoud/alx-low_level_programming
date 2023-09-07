abae4c7d0dce97c424f7e46253ce7279e0864d1b
Sarahgad <sarahgad91@gmail.com>
2023-09-07 13:21:59 +0300

    0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include<stdlib.h>
+#include "hash_tables.h"
+
+/**
+ *
+ * 
+ * 
+ * 
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+   unsigned int i;
+    hash_table_t *hash;
+    hash = malloc(sizeof(hash_table_t));
+    if(hash == NULL)
+    return (NULL);
+
+    hash->array = malloc(sizeof(hash_node_t) * size);
+    if(hash->array == NULL)
+{
+    free(hash);
+    return(NULL);
+}
+
+    for (i = 0; i < size; i++)
+    {
+        hash->array[i] = NULL;
+    }
+    hash->size = size;
+return (hash);     
+}
