73b72469a35a8366c1cdc350e6cd3b4e8e90397a
Sarahgad <sarahgad91@gmail.com>
2023-09-08 21:53:13 +0300

    0

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,26 @@
+#include"hash_tables.h"
+/**
+*hash_table_print - print the hash table
+*@ht: the hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned long int i;
+hash_node_t *temp;
+
+if (ht == NULL)
+return;
+
+for (i = 0; i < ht->size; i++)
+{
+
+	temp = ht->array[i];
+	while (temp)
+	{
+	free(ht->array[i]);
+	temp = temp->next;
+	ht->array[i] = temp;
+	}
+	free(ht->array[i]);
+}
+}
