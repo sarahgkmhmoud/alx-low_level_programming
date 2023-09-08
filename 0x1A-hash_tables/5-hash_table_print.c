269bddccdfeb46d8364925d9b6e2a3f1f2d6da0c
Sarahgad <sarahgad91@gmail.com>
2023-09-08 21:39:59 +0300

    0

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,6 +8,10 @@ void hash_table_print(const hash_table_t *ht)
 unsigned long int i;
 int count = 0;
 hash_node_t *temp;
+
+if (ht == NULL)
+return;
+
 printf("{");
 for (i = 0; i < ht->size; i++)
 {
