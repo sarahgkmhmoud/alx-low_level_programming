bfa3d4ef5d5ac6a544a2679c6b657fd63fed9205
Sarahgad <sarahgad91@gmail.com>
2023-09-08 18:19:03 +0300

    4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,33 +1,5 @@
 #include"hash_tables.h"
 
-// int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-// {
-//     hash_node_t **current;
-//     long unsigned int index;
-//     if (key == NULL || value == NULL)
-//     return (0);
-
-//     index = key_index((const unsigned char *)key, ht->size);
-    
-//     current = malloc(sizeof(hash_node_t *));
-//         if (current == NULL) 
-//                 return (0);
-
-//     current[index]->next = NULL;
-//     current[index]->key = strdup(key);
-//     current[index]->value =strdup(value);
-//     if (ht->array[index] == NULL)
-//     {
-//         *current = ht->array[index];
-//     }
-//     else
-//     {
-//     current[index]->next = ht->array[index];
-//     ht->array[index] = *current;
-//     }
-//     return (1);
-// }
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
     hash_node_t *current;
