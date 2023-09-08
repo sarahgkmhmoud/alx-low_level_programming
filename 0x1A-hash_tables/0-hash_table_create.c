8ee29b6c6d666c0bfb71f3a72f4ad42a9f4154ee
Sarahgad <sarahgad91@gmail.com>
2023-09-08 14:41:54 +0300

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,10 +18,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash->array = malloc(sizeof(hash_node_t) * size);
 	if (hash->array == NULL)
-{
-	free(hash);
+	{
 	return (NULL);
-}
+	}
 
 	for (i = 0; i < size; i++)
 	{
