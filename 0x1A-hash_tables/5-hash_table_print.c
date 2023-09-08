2998894103b7c6d1c09e827d026afcf201ffa681
Sarahgad <sarahgad91@gmail.com>
2023-09-08 21:25:41 +0300

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,15 +1,17 @@
 #include"hash_tables.h"
-
+/**
+*hash_table_print - print the hash table
+*@ht: the hash table
+*/
 void hash_table_print(const hash_table_t *ht)
 {
 unsigned long int i;
+printf("{");
 for (i = 0; i < ht->size; i++)
 {
-    if(ht->array[i] == NULL)
-    continue;
-    if (i <= ht->size - 1)
-    printf("'%s': '%s', ", ht->array[i]->key, ht->array[i]->value);
-    else if (i == ht->size -1 && ht->array[i] != NULL)
-    printf("'%s': '%s'}", ht->array[i]->key, ht->array[i]->value);
+	if (ht->array[i] && i < ht->size - 1)
+	printf("\'%s\': \'%s\', ", ht->array[i]->key, ht->array[i]->value);
+	else if (ht->array[i] && i == ht->size - 1)
+	printf("\'%s\': \'%s\'}", ht->array[i]->key, ht->array[i]->value);
 }
 }
