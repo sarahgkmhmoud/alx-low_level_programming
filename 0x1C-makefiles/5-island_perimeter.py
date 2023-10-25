9e3c8a3c7a3f21665b2992fd1d031ada772012fa
Sarahgad <sarahgad91@gmail.com>
2023-10-25 12:09:26 +0300

    README, 0 , 1, 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""technical interview"""
+
+
+def island_perimeter(grid):
+    """   width_list = []
+    height = 0
+    for list in grid:
+        width_pot = 0
+        for item in list:
+            if item == 1:
+                width_pot += 1
+        width_list.append(width_pot)
+        if width_pot:
+            height += 1
+
+    width = max(width_list)
+    perimeter = (width + height) * 2
+
+    return perimeter
+"""
+    width_list = []
+    height = 0
+    for list in grid:
+        width_calc = sum(list)
+        width_list.append(width_calc)
+        if width_calc:
+            height += 1
+    width = max(width_list)
+    perimeter = (width + height) * 2
+
+    return perimeter
