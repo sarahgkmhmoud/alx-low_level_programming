745a2b40afd5b67183c3bde0996df0d4a98b89b1
Sarahgad <sarahgad91@gmail.com>
2023-10-25 12:16:08 +0300

    Try it

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -18,8 +18,10 @@ def island_perimeter(grid):
     perimeter = (width + height) * 2
 
     return perimeter
-    Seconde sol:
-    "
+"""
+    if len(grid) == 1:
+       return 1
+
     width_list = []
     height = 0
     for list in grid:
@@ -27,22 +29,6 @@ def island_perimeter(grid):
         width_list.append(width_calc)
         if width_calc:
             height += 1
-    width = max(width_list)
-    perimeter = (width + height) * 2
-
-    return perimeter
-"""
-    width_list = []
-    height = 0
-    for list in grid:
-        width_pot = 0
-        for item in list:
-            if item == 1:
-                width_pot += 1
-        width_list.append(width_pot)
-        if width_pot:
-            height += 1
-
     width = max(width_list)
     perimeter = (width + height) * 2
 
