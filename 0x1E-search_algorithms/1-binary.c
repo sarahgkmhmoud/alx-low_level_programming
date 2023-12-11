6a050c5f8de08ea580b3b893dbd0fd09f6d3426b
Sarahgad <sarahgad91@gmail.com>
2023-12-12 00:40:09 +0200

    0

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+/**
+ *binary_search - function that searches for a value in an array of integers
+ *@array: pointer to the first element of the array to search in
+ *@size: the number of elements in array
+ *@value:  the value to search for
+ *Return: i alway success
+*/
+size_t recur_binary_search(start, end){
+    if (start > end || !array)
+    return -1;
+    
+
+
+
+}
+int binary_search(int *array, size_t size, int value){
+if (array)
+{
+   
+}
+}
