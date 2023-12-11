ea530b28a7060ec5ce94bb37ad765e9dc997b310
Sarahgad <sarahgad91@gmail.com>
2023-12-12 01:03:07 +0200

    1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,4 +1,22 @@
 #include "search_algos.h"
+void print_binary_search(int *array, size_t start, size_t end);
+/**
+ *print_binary_search - print the array
+ *@array: array
+ *@start: start
+ *@end: end
+*/
+void print_binary_search(int *array, size_t start, size_t end)
+{
+	printf("Searching in array: ");
+	while (start < end)
+	{
+		printf("%i, ", array[start]);
+		start++;
+	}
+	printf("%i\n", array[end]);
+}
+
 /**
  *binary_search - function that searches for a value in an array of integers
  *@array: pointer to the first element of the array to search in
@@ -6,17 +24,26 @@
  *@value:  the value to search for
  *Return: i alway success
 */
-size_t recur_binary_search(start, end){
-    if (start > end || !array)
-    return -1;
-    
-
-
-
-}
-int binary_search(int *array, size_t size, int value){
+int binary_search(int *array, size_t size, int value)
+{
 if (array)
 {
-   
+	size_t start, end, mid;
+
+	start = 0;
+	end = size - 1;
+	while (start <= end)
+	{
+		mid = start + ((end - start) / 2);
+		print_binary_search(array, start, end);
+		if (array[mid] == value)
+		return (mid);
+		else if (array[mid] > value)
+		end = mid - 1;
+		else if (array[mid] < value)
+		start = mid + 1;
+	}
+
 }
+return (-1);
 }
