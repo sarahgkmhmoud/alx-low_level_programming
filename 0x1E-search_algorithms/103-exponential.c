6087995dc09aa9c2eb0db627a33af9bbb695b7ac
Sarahgad <sarahgad91@gmail.com>
2023-12-13 00:11:06 +0200

    103

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -5,17 +5,17 @@ int min(int a, int b);
 void print_binary_search(int *array, size_t start, size_t end);
 
 /**
- *min - find minmum
+ *find_min - find minmum
  *@a: a
  *@b: b
- *Rerturn: i 
+ *Return: a, b
 */
 int find_min(int a, int b)
 {
-    if(b>a)
-      return a;
-      else
-      return b;
+if (b > a)
+	return (a);
+else
+	return (b);
 }
 /**
  *exponential_search - jump search
@@ -27,47 +27,49 @@ int find_min(int a, int b)
 int exponential_search(int *array, size_t size, int value)
 {
 
-    int i;
+	int i;
 
-    if (!array)
+	if (!array)
 		return (-1);
-    if (array[0] == value)
-        return (0);
+	if (array[0] == value)
+		return (0);
 i = 1;
 while (i < (int)size && array[i] <= value)
 {
-    printf("Value checked array[%d] = [%d]\n", i, array[i]);
-    i = i * 2;
-    }
-    printf("Value found between indexes [%d] and [%d]\n", i/2, find_min(i, size - 1));
+	printf("Value checked array[%d] = [%d]\n", i, array[i]);
+	i = i * 2;
+	}
+	printf("Value found between indexes [%d] and [%d]\n", i / 2,
+	find_min(i, size - 1));
 
-    return rec_binary_search(array, i/2, find_min(i, size - 1), value);
+	return (rec_binary_search(array, i / 2, find_min(i, size - 1), value));
 }
 /**
- * 
- * 
- * 
- * 
- * 
+ *rec_binary_search - Recursion
+ *@array: array
+ *@index: index
+ *@step: step
+ *@value: value
+ *Return: 1
 */
 int rec_binary_search(int *array, int index, int step, int value)
 {
 int mid;
-    if (step >= index)
-    {
-        print_binary_search(array, index, step);
+	if (step >= index)
+	{
+		print_binary_search(array, index, step);
 
-        mid = index + (step - index) / 2;
-        if (array[mid] == value)
-        return (mid);
+		mid = index + (step - index) / 2;
+		if (array[mid] == value)
+		return (mid);
 
-        if (array[mid] > value)
-        return (rec_binary_search(array, index, mid - 1, value));
+		if (array[mid] > value)
+		return (rec_binary_search(array, index, mid - 1, value));
 
-        return (rec_binary_search(array, mid + 1, step, value));
+		return (rec_binary_search(array, mid + 1, step, value));
 
-    }
-    return (-1);
+	}
+	return (-1);
 }
 /**
  *print_binary_search - print the array
